cfd933efed1dab1605bb2b57ccf11eb331751500
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-03-03 20:26:38 +0100

    add extensions and requirements.txt

--- hangman.py
+++ hangman.py
@@ -1,26 +1,29 @@
 
 import random
 from words import words 
+from pprint import pprint
 
-#from colorama import Fore, Back, Style
-#import pyfiglet
+import colorama
+from colorama import Fore, Back, Style
+import pyfiglet
 
-#colorama.init()
+colorama.init()
 
-#result = pyfiglet.figlet_format("H A N G M A N", font= "alligator")
-#print(colorama.Fore.RED + result)
 
+result = pyfiglet.figlet_format(" H A N G M A N ", font= "alligator")
+print(colorama.Back.CYAN + colorama.Fore.GREEN + colorama.Style.BRIGHT + result)
+"""
 def hangman(words):
 #start with a number of lives
 
 # when play is pushed, start the game
-
+playgame():
 # choose a random word from the words file and show the places of the total number of letters
-
+word = random.choice(words).upper
 # let words all be uppercase
 
 # let the player choose a letter for the word and make it uppercase
-
+input = ("Pick a letter: ").upper
 # make sure it is just a letter - return a wrong message if a non-letter was typed
 
 # if letter is right fill out letter in the right place
@@ -36,7 +39,9 @@ def hangman(words):
 # print a message with the results
 
 
+#hangman()
 
-hangman()
+def playgame()
 
-print("world")
+"""
+
